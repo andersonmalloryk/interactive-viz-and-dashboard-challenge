4afa47bedf99d3933fda0da4bf8348256479740f
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-04-18 16:17:40 -0500

    have individual sample reading for first graph

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,11 @@ console.log("app.js is loaded")
 // create a stub for the functions that will be created
 function DrawBarGraph(sampleID) {
     console.log(`DrawBarGraph(${sampleID})`);
+
+    d3.json("data/samples.json").then(data => {
+        var samples = data.samples;
+        var resultsArray = samples.filter(s => s.id == sampleID);
+    });
 }
 
 function DrawBubbleChart(sampleID) {
@@ -15,28 +20,26 @@ function ShowMetaData(sampleID) {
 
 d3.selectAll("#selDataset").on("change", optionChanged);
 
-// function optionChanged(){
-//     var selector = d3.select("#selDataset");
-//     var sampleID = selector.property("value");
-// };
 function optionChanged() {
     console.log("optionChanged")
     var selector = d3.select("#selDataset");
     var sampleID = selector.property("value")
 
+    // update bar graph
     DrawBarGraph(sampleID);
+    //update bubble chart
     DrawBubbleChart(sampleID);
+    //update demographic information
     ShowMetaData(sampleID);
 };
 
-function InitDashboard(){
+function InitDashboard() {
     console.log("InitDashboard()");
 
     // populate dropdown with test subject id numbers
     var selector = d3.select("#selDataset");
 
-    d3.json("data/samples.json").then(data =>{
-        console.log(data);
+    d3.json("data/samples.json").then(data => {
         var sampleNames = data.names;
 
         sampleNames.forEach(sampleID => {
@@ -44,13 +47,12 @@ function InitDashboard(){
                 .text(sampleID)
                 .property("value", sampleID)
         });
-    })
+        var id = sampleNames[0]
 
-    // update bar graph
+        DrawBarGraph(id);
+        DrawBubbleChart(id);
+        ShowMetaData(id);
+    });
+};
 
-    //update bubble chart
-
-    //update demographic information
-}
-
-InitDashboard();
+    InitDashboard();
